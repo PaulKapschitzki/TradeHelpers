33d1f33e03845d17815bcbe9dba7262d5356ef83
PaulKapschitzki <kapschitzki.p@web.de>
2020-10-06 10:07:34 +0200

    Changed Rating for BuySetup

--- render_BS.js
+++ render_BS.js
@@ -29,7 +29,7 @@ window.onload = function(){
         if( testCheckbox(document.getElementById("bsRetracement")) ) {
             rating_BS += 20;
         } else {
-            rating_BS -= 20;
+            rating_BS -= 0;
         }
 
         // Retracement to Moving Average?
@@ -41,14 +41,14 @@ window.onload = function(){
 
         // 50% Retracement?
         if( testCheckbox(document.getElementById("bsRetracement50perCent")) ) {
-            rating_BS += 5;
+            rating_BS += 10;
         } else {
             rating_BS -= 0;
         }
 
         // Retracement to prior Pivot or Support / Resistance?
         if( testCheckbox(document.getElementById("bsRetracementPivot")) ) {
-            rating_BS += 5;
+            rating_BS += 10;
         } else {
             rating_BS -= 0;
         }
@@ -57,19 +57,19 @@ window.onload = function(){
         if( testCheckbox(document.getElementById("bsIndecisionCandles")) ) {
             rating_BS += 10;
         } else {
-            rating_BS -= 10;
+            rating_BS -= 0;
         }
 
         // Retest & Failure?
         if( testCheckbox(document.getElementById("bsRetestAndFailure")) ) {
-            rating_BS += 5;
+            rating_BS += 10;
         } else {
             rating_BS -= 0;
         }
 
         // Ending Volume
         if( testCheckbox(document.getElementById("bsEndingVolume")) ) {
-            rating_BS += 5;
+            rating_BS += 10;
         } else {
             rating_BS -= 0;
         }
@@ -78,14 +78,14 @@ window.onload = function(){
         if( testCheckbox(document.getElementById("bsBottomingTails")) ) {
             rating_BS += 10;
         } else {
-            rating_BS -= 10;
+            rating_BS -= 0;
         }
 
         // V-shape top of prior Pivot
         if( testCheckbox(document.getElementById("bsVShapePivot")) ) {
             rating_BS += 5;
         } else {
-            rating_BS -= 5;
+            rating_BS -= 0;
         }
 
         // Results
@@ -93,10 +93,12 @@ window.onload = function(){
 
         if ( rating_BS <= 70 ) {
             formResult = "Bad Trade!";
-        } else if ( rating_BS > 70 && rating_BS <= 90 ) {
+        } else if ( rating_BS > 70 && rating_BS <= 85 ) {
             formResult = "Good Trade!";
-        } else if ( rating_BS > 90 ) {
+        } else if ( rating_BS > 85 && rating_BS < 100 ) {
             formResult = "Great Trade!";
+        } else if ( rating_BS >= 100 ) {
+            formResult = "Awesome Trade! I would take it!";
         } else {
             formResult = "ERROR: No result available!";
         }
