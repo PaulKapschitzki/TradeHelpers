dfa102ff3926c438f7817676bf07a1da41918605
PaulKapschitzki <kapschitzki.p@web.de>
2020-10-13 19:16:51 +0200

    Added new Checklist Setup

--- renders/render_2LM.js
+++ renders/render_2LM.js
@@ -1,148 +1,121 @@
 "use strict";
 window.onload = function(){
     // Check buttons
-    const checkBtn_BO   = document.getElementById("checkBtn_BO");
+    const checkBtn_2LM   = document.getElementById("checkBtn_2LM");
 
     // Reset buttons
-    const resetBtn_BO = document.getElementById("resetBtn_BO");
+    const resetBtn_2LM = document.getElementById("resetBtn_2LM");
 
     // form elements BS
-    const resultText_BO = document.getElementById("boResultText");
-    const form_BO = document.getElementById("form_BO");
-    const ratingText_BO = document.getElementById("boRatingText");
-    const chkBxboNearRoundNumber_BO = document.getElementById("boNearRoundNumber");
+    const resultText_2LM = document.getElementById("2lmResultText");
+    const form_2LM = document.getElementById("form_2LM");
+    const ratingText_2LM = document.getElementById("2lmRatingText");
+    const chkBxboNearRoundNumber_2LM = document.getElementById("2lmNearRoundNumber");
 
     // Ratings
-    let rating_BO   = 0;
+    let rating_2LM   = 0;
 
     /**
      * @description Checks the results BuySetup/SellSetup checklist
      */
-    const checkBOTrade = () => {
-        // Consolidation before setup with at least 5 Bars?
-        if( testCheckbox(document.getElementById("boConsolidation")) ) {
-            rating_BO += 25;
+    const check2LMTrade = () => {
+        // Strong initial move with 2 - 5 Bars?
+        if( testCheckbox(document.getElementById("2lmStrongIgniting")) ) {
+            rating_2LM += 20;
         } else {
-            rating_BO -= 0;
+            rating_2LM -= 0;
         }
 
-        // Relatively low volume during consolidation?
-        if( testCheckbox(document.getElementById("boConsolLowVol")) ) {
-            rating_BO += 20;
+        // Initial move comes after a retracement?
+        if( testCheckbox(document.getElementById("2lmPriorConsolidation")) ) {
+            rating_2LM += 10;
         } else {
-            rating_BO -= 0;
+            rating_2LM -= 0;
         }
 
-        // Relative Strength?
-        if( testCheckbox(document.getElementById("boRS")) ) {
-            rating_BO += 5;
+        // The 1st leg hast to be smaller than 15 of stock price?
+        if( testCheckbox(document.getElementById("2lmSmallFirstLeg")) ) {
+            rating_2LM += 5;
         } else {
-            rating_BO -= 0;
+            rating_2LM -= 0;
         }
 
-        // 21 EMA retracement?
-        if( testCheckbox(document.getElementById("bo21EmaRetracement")) ) {
-            rating_BO += 10;
+        // Short Consolidation with no or small retracement = Inside Resting Bar??
+        if( testCheckbox(document.getElementById("2lmInsideRestingBar")) ) {
+            rating_2LM += 15;
         } else {
-            rating_BO -= 0;
+            rating_2LM -= 0;
         }
 
-        // Clean and Tight base?
-        if( testCheckbox(document.getElementById("boCleanAndTightBase")) ) {
-            rating_BO += 15;
+        // Consolidation only 1 (max. 2) Bars?
+        if( testCheckbox(document.getElementById("2lmIRB1or2Bars")) ) {
+            rating_2LM += 15;
         } else {
-            rating_BO -= 0;
+            rating_2LM -= 0;
         }
 
-        // Big Round Number
-        let roundNumbers = document.getElementsByName("numberType");
-        let numberType = "";
-
-        for(var i = 0; i < roundNumbers.length; i++)
-        {
-            if(roundNumbers[i].checked)
-            {
-                numberType = roundNumbers[i].value;
-                if (numberType === "VBRN") rating_BO += 15;
-                if (numberType === "BRN") rating_BO += 10;
-                if (numberType === "HRN") rating_BO += 5;
-            }
+        // Does IRB have low volume (NO high Ending Volume)?
+        if( testCheckbox(document.getElementById("2lmIRBLowVolume")) ) {
+            rating_2LM += 15;
+        } else {
+            rating_2LM -= 0;
         }
 
-        // Retest & Failure?
-        if( testCheckbox(document.getElementById("boTrendlineBreak")) ) {
-            rating_BO += 5;
+        // Does IRB make a 50% retracement of prior bar?
+        if( testCheckbox(document.getElementById("2LMIRB50PercentRetracement")) ) {
+            rating_2LM += 5;
         } else {
-            rating_BO -= 0;
+            rating_2LM -= 0;
         }
 
-        // Ending Volume
-        if( testCheckbox(document.getElementById("boEasy2See")) ) {
-            rating_BO += 5;
+        // Does IRB form in upper 50% of prior bar?
+        if( testCheckbox(document.getElementById("2lmIRBUpper50Percent")) ) {
+            rating_2LM += 10;
         } else {
-            rating_BO -= 0;
+            rating_2LM -= 0;
+        }
+
+        // Does IRB have Bottoming Tails?
+        if( testCheckbox(document.getElementById("2lmIRBBottomingTails")) ) {
+            rating_2LM += 5;
+        } else {
+            rating_2LM -= 0;
         }
 
         // Results
         let formResult = "";
 
-        if ( rating_BO <= 70 ) {
+        if ( rating_2LM <= 70 ) {
             formResult = "Bad Trade!";
-        } else if ( rating_BO > 70 && rating_BO <= 85 ) {
+        } else if ( rating_2LM > 70 && rating_2LM <= 85 ) {
             formResult = "Good Trade!";
-        } else if ( rating_BO > 85 && rating_BO < 100 ) {
+        } else if ( rating_2LM > 85 && rating_2LM < 100 ) {
             formResult = "Great Trade!";
-        } else if ( rating_BO >= 100 ) {
+        } else if ( rating_2LM >= 100 ) {
             formResult = "Awesome Trade! I would take it!";
         } else {
             formResult = "ERROR: No result available!";
         }
 
         // Displaying Results
-        resultText_BO.innerHTML = formResult;
-        ratingText_BO.innerHTML = rating_BO;
-        console.log("rating-BS: " + rating_BO);
+        resultText_2LM.innerHTML = formResult;
+        ratingText_2LM.innerHTML = rating_2LM;
     };
 
     // Check BO/BD form inputs
-    checkBtn_BO.onclick = () => {
+    checkBtn_2LM.onclick = () => {
         // reset ratings
-        rating_BO = 0;
+        rating_2LM = 0;
 
-        checkBOTrade();
+        check2LMTrade();
     };
 
     // Clear Rating, Results and Form Elements of BO form
-    resetBtn_BO.onclick = () => {
-        form_BO.reset();
-        resultText_BO.innerHTML = "";
-        ratingText_BO.innerHTML = "";
-        rating_BO = 0;
-    }
-
-    /**
-     * @description Enables or disables numberType HTML elements according to value of a the "boNearRoundNumber" checkbox
-     */
-    chkBxboNearRoundNumber_BO.onclick = () => {
-        let vbrn = document.getElementById("boVBRN");
-        let brn = document.getElementById("boBRN");
-        let hrn = document.getElementById("boHRN");
-        
-        if (chkBxboNearRoundNumber_BO.checked == true) {
-            // enable radio buttons
-            vbrn.disabled = false;
-            brn.disabled = false;
-            hrn.disabled = false;
-        } else {
-            // clear selection
-            vbrn.checked = false;
-            brn.checked = false;
-            hrn.checked = false;
-            // disable radio buttons
-            vbrn.disabled = true;
-            brn.disabled = true;
-            hrn.disabled = true;
-        }
+    resetBtn_2LM.onclick = () => {
+        form_2LM.reset();
+        resultText_2LM.innerHTML = "";
+        ratingText_2LM.innerHTML = "";
+        rating_2LM = 0;
     }
 
     /**
