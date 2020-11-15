8e2f9366eb61afee67db6a36b10ebea998c637fb
PaulKapschitzki <kapschitzki.p@web.de>
2020-11-15 11:03:34 +0100

    Added function to store userData in AppData Test for saving resized window size

--- main.js
+++ main.js
@@ -1,15 +1,39 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require('electron');
+const Store = require('./store');
+
+// First instantiate the class
+const store = new Store({
+    // We'll call our data file 'user-preferences'
+    configName: 'user-preferences_tradeHelpers',
+    defaults: {
+      // 800x600 is the default size of our window
+      windowBounds: { width: 600, height: 950 }
+    }
+});
 
 function createWindow () {
+    // First we'll get our height and width. This will be the defaults if there wasn't anything saved
+    let { width, height } = store.get('windowBounds');
+
     // create browser window.
     const win = new BrowserWindow({
-        width: 600,
-        height: 950,
+        width: width,
+        height: height,
         webPreferences: {
             nodeIntegration: true
         },
         resizable: false
     })
+
+    // The BrowserWindow class extends the node.js core EventEmitter class, so we use that API
+    // to listen to events on the BrowserWindow. The resize event is emitted when the window size changes.
+    win.on('resize', () => {
+        // The event doesn't pass us the window size, so we call the `getBounds` method which returns an object with
+        // the height, width, and x and y coordinates.
+        let { width, height } = win.getBounds();
+        // Now that we have them, save them using the `set` method.
+        store.set('windowBounds', { width, height });
+    });
 
     // and load the index.html of the app.
     win.loadFile('index.html')
@@ -20,7 +44,7 @@ function createWindow () {
     // win.webContents.openDevTools()
 
     // remove menu bar with false, show with true
-    win.setMenuBarVisibility(false);
+    win.setMenuBarVisibility(true);
 }
 
 // This method will be called when Electron has finished
